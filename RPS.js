86b0a111bae42b77f2b8f5d759797901e40923b2
rabbit.hol399 <rabbit.hol399@gmail.com>
2024-09-05 18:40:01 -0600

    upload previous

---
+++ RPS.js
@@ -0,0 +1,12 @@
+let humanscore = 0
+let compscore = 0
+const choice = ["rock, paper, scissors"]
+function getComputerChoice () {
+    return Math.floor(Math.random() * max);
+}
+
+function getRandomArbitrary(min, max) {
+    return Math.random() * (max-min) + min;
+    
+}
+console.log (getRandomArbitrary(3, 9))
